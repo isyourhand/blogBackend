ed871fe022fdd32d26f5467c359068c77064e8e6
LYX <853538749@qq.com>
2024-02-28 15:39:13 +0800

    update

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const app = require("./app.js");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+const fs = require("fs");
+const https = require("https");
+
 dotenv.config({ path: "./.env" });
 
 const db = process.env.DATABASE;
@@ -10,10 +13,25 @@ mongoose.connect(db).then(() => {
   console.log("MongoDb Connection Successful.");
 });
 
-const hostName =
-  process.env.NODE_ENV === "development" ? "127.0.0.1" : "0.0.0.0";
-console.log(hostName);
-const port = 4000;
-const server = app.listen(port, hostName, () => {
-  console.log(`App running on port ${port}...`);
-});
+const port = 4000; // 默认的HTTPS端口号为443
+
+if (process.env.NODE_ENV === "development") {
+  const hostName =
+    process.env.NODE_ENV === "development" ? "127.0.0.1" : "0.0.0.0";
+  console.log(hostName);
+
+  const server = app.listen(port, hostName, () => {
+    console.log(`App running on port ${port}...`);
+  });
+} else {
+  const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/llog.top/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/llog.top/fullchain.pem"),
+  };
+
+  const server = https.createServer(options, app);
+
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
